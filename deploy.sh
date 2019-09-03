1ed69d7a5b59d1a81fa6ef9ae073125e3a9c7050
Denis Sulgin <sulgin.ds@med54.ru>
2019-09-03 13:26:35 +0700

    fix wget command

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-wget -O https://raw.githubusercontent.com/Denriful/cloud-testapp/master/install_ruby.sh | bash
+wget -O - https://raw.githubusercontent.com/Denriful/cloud-testapp/master/install_ruby.sh | bash
 
-wget -O https://raw.githubusercontent.com/Denriful/cloud-testapp/master/install_mongodb.sh | bash
+wget -O - https://raw.githubusercontent.com/Denriful/cloud-testapp/master/install_mongodb.sh | bash
 
 git clone -b monolith https://github.com/express42/reddit.git
 
