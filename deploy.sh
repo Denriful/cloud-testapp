13537daad2e4d38422f732cafd2e5af4cd9b78ee
Denis Sulgin <sulgin.ds@med54.ru>
2019-09-03 09:40:31 +0700

    edit startup script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+wget -O https://raw.githubusercontent.com/Denriful/cloud-testapp/master/install_ruby.sh | bash
+
+wget -O https://raw.githubusercontent.com/Denriful/cloud-testapp/master/install_mongodb.sh | bash
+
 git clone -b monolith https://github.com/express42/reddit.git
 
 cd reddit && bundle install
