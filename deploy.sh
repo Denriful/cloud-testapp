afc069353f4f0fb71623acaea03ee22119e371c3
Denis Sulgin <sulgin.ds@med54.ru>
2019-09-03 13:48:16 +0700

    fix sudo in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,8 @@ wget -O - https://raw.githubusercontent.com/Denriful/cloud-testapp/master/instal
 
 wget -O - https://raw.githubusercontent.com/Denriful/cloud-testapp/master/install_mongodb.sh | bash
 
-git clone -b monolith https://github.com/express42/reddit.git
+sudo git clone -b monolith https://github.com/express42/reddit.git
 
-cd reddit && bundle install
+sudo cd reddit && sudo bundle install
 
-puma -d
+sudo puma -d
