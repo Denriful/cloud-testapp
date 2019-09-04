6940619cbbf99729ddbaa30e1c03507395fbbfbb
Denis Sulgin <sulgin.ds@med54.ru>
2019-09-04 09:35:40 +0700

    final check1

--- deploy.sh
+++ deploy.sh
@@ -6,22 +6,22 @@ wget -O - https://raw.githubusercontent.com/Denriful/cloud-testapp/master/instal
 
 git clone -b monolith https://github.com/express42/reddit.git
 
-# sudo cd /reddit && sudo bundle install
+cd /reddit && bundle install && puma -d
 
-cd /reddit
+# cd /reddit
 
-touch deploy.log
+# touch deploy.log
 
-echo 'current dir after "cd" command: '$PWD > deploy.log
+# echo 'current dir after "cd" command: '$PWD > deploy.log
 
-sudo bundle install
+# sudo bundle install
 
-echo 'bundle install exit status: '$? >> deploy.log
+# echo 'bundle install exit status: '$? >> deploy.log
 
-sudo puma -d
+# sudo puma -d
 
-echo 'puma install exit status: '$? >> deploy.log
+# echo 'puma install exit status: '$? >> deploy.log
 
-sudo apt install mc
+# sudo apt install mc
 
-echo 'mc install exit status: '$? >> deploy.log
+# echo 'mc install exit status: '$? >> deploy.log
